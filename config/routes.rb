7f95d689e7850c52d829e5460b074e039f7fee0e
Mehdi Seffar <mehdi.seffar@outlook.fr>
2021-04-26 16:59:03 +0200

    adding boostrap 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :properties
-  devise_for :accounts
   root to: 'public#main'
+    resources :properties
+    devise_for :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
